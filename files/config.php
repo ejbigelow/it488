41e835a6fc4ab3b581d617ac33ef6b6a373c9367
Eric Bigelow <ericbigelow@student.kaplan.edu>
2017-03-29 20:45:41 -0400

    Create config.php

---
+++ files/config.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: serrin
+ * Date: 3/29/17
+ * Time: 1:16 PM
+ */
+
+/**
+ * THIS IS THE PRIMARY MACHINE CONFIGURATION THAT WILL ALLOW MULTIPLE MACHINES ACT INDEPENDENTLY PLEASE TYPE YOUR SEQUEL
+ * SERVER PASSWORD BELOW.
+ *
+ */
+
+//Do not change sqlUser
+$sqlUser = 'root';
+
+//Please change the password below to the password your sequel server is using
+$sqlPassword = 'Password_here';
