b9195dae9c7b84c77fdf5c711a77d883eb11d720
Eric Bigelow <eric.bigelow@me.com>
2017-04-14 14:30:59 -0400

    Yenny's form insertion

---
+++ lib/registration.php
@@ -0,0 +1,83 @@
+        <form>
+            <label for="firstName" class="control-label">Name: </label>
+            <div class="row padding-top-10">
+                <div class ="col-md-6">
+                    <input type="text" class="form-control" id="firstname" placeholder="First"/>
+
+                </div>
+            </div>
+            <div class="row padding-top-10">
+                <div class ="col-md-6">
+                    <input type="text" class="form-control" id="Lastname" placeholder="Last"/>
+
+                </div>
+            </div>
+
+            <label for= "username" class="control-label">Username : </label>
+            <div class="row padding-top-10">
+                <div class ="col-md-6">
+                    <input type="text" class="form-control" id="username" placeholder="username"/>
+
+                </div>
+            </div>
+
+            <label for="password" class="control-label">Password: </label>
+            <div class="row padding-top-10">
+                <div class ="col-md-6">
+                    <input type="text" class="form-control" id="password" placeholder="password"/>
+
+                </div>
+            </div>
+
+            <label for="cpassword" class="control-label">confirm Password: </label>
+            <div class="row padding-top-10">
+                <div class ="col-md-6">
+                    <input type="text" class="form-control" id="cpassword" placeholder="confirm password"/>
+
+                </div>
+            </div>
+
+            <label for="email" class="control-label">Email: </label>
+            <div class="row padding-top-10">
+                <div class ="col-md-6">
+                    <input type="text" class="form-control" id="email" placeholder="e.g @hotmail.com"/>
+
+                </div>
+            </div>
+
+            <label for ="address1" class="control-label padding-top-10">Address:</label>
+
+            <div class ="row padding-top-10">
+                <div class="col-md-12">
+                    <input type= "text" class="form-control" id ="address1" placeholder="Address line 1"/>
+                </div>
+            </div>
+
+            <div class ="row padding-top-10">
+                <div class="col-md-12">
+                    <input type= "text" class="form-control" id ="address2" placeholder="Address line 2"/>
+
+                </div>
+            </div>
+
+            <div class="row padding-top-10">
+                <div class="col-md-6">
+                    <label for ="city" class="control-label">City:</label>
+                    <input type ="text" class="form-control" id="city" placeholder="your city"/>
+                </div>
+            </div>
+            <div class="row padding-top-10">
+                <div class="col-md-2">
+                    <label for="stateorregion" class="control-label">State/Region:</label>
+                    <input type ="text" class="form-control" id="stateorregion" placeholder="your state/region"/>
+                </div>
+            </div>
+            <div class="row padding-top-10">
+                <div class="col-md-4">
+                    <label for="zipcode" class="control-label">Zipcode:</label>
+                    <input type ="text" class="form-control" id="zipcode" placeholder="your zipcode"/>
+
+                </div>
+            </div>
+
+        </form>
