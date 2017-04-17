8bcaa9f907ce77f513455f265f007603d0c8ab44
Eric Bigelow <eric.bigelow@me.com>
2017-04-17 13:02:27 -0400

    dismissable success alert

--- bin/formHandler.php
+++ bin/formHandler.php
@@ -6,12 +6,21 @@
  * Time: 12:21 PM
  */
 var_dump($_POST);
-if (isset($_POST['submit'])) {
-    $alert = 1;
-    $type ="alert-success text-success";
-    $text = "You have successfully registered!";
+if (isset($_POST['registration']) || isset($_POST['logon'])) {
+    if (isset($_POST['registration'])) {
+        $alert = 1;
+        $type ="alert-success text-success alert-dismissible";
+        $text = "You have successfully registered!";
+    }
+    elseif (isset($_POST['logon'])) {
+
+    }
+    else {
+        $alert = 1;
+        $type = "alert-danger text-danger";
+        $text = "an unknown result was triggered in". $_SERVER['PHP_SELF'];
+    }
 }
 else {
-        echo $_SERVER['PHP_SELF'] .":cancelled";
 }
 ?>
