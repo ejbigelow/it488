531de763115409617b0d3db6d3092036a7736082
Eric Bigelow <eric.bigelow@me.com>
2017-04-20 15:13:14 -0400

    working salt

--- bin/formHandler.php
+++ bin/formHandler.php
@@ -6,33 +6,37 @@
  * Time: 12:21 PM
  */
 
-    if (isset($_POST['registration']) || isset($_POST['logon'])) {
-        if (isset($_POST['registration'])) {
-            /* associated post variables returned sanitized*/
-            $fname = sanitize($_POST['fname'], null);
-            $lname = sanitize($_POST['lname'], null);
-            $username = sanitize($_POST['username'], null);
-            $email = sanitize($_POST['email'], null);
-            $address1 = sanitize($_POST['address1'], null);
-            $city = sanitize($_POST['city'], null);
-            $state = sanitize($_POST['state'], null);
-            $zip = sanitize($_POST['zip'], null);
-            /* unsanitized */
-            $password = $_POST['password'];
-            $password_confirm = $_POST['password2'];
-            if ($password != $password_confirm) {
-                $alert = 1;
-                $type = "alert-danger text-danger alert-dismissible";
-                $text = "Your passwords must match!";
-            }
-            else {
-                include INC_ROOT . 'bin/sqlConnector.php';
-                /* Encrypt password into database */
-                $encryptedPassword = md5($password);
-                $textPassword = "$encryptedPassword";
-                //$sql = "INSERT INTO users (FirstName,LastName,username,Password,email,address1,City,state, zipcode, joined) VALUES ( {$fname},{$lname},{$username},{$textPassword},{$email},{$address1},{$city},{$state},{$zip},NOW())";
-                $sql = "INSERT INTO users (FirstName,LastName,username,Password,email,address1,City,state, zipcode, joined) VALUES ('$fname', '$lname','$username','$password','$email','$address1','$city','$state','$state', NOW())";
+if (isset($_POST['registration']) || isset($_POST['logon'])) {
+    if (isset($_POST['registration'])) {
+        include INC_ROOT . 'bin/sqlConnector.php';
+        /* associated post variables returned sanitized*/
+        $fname = sanitize($_POST['fname'], null);
+        $lname = sanitize($_POST['lname'], null);
+        $username = sanitize($_POST['username'], null);
+        $email = sanitize($_POST['email'], null);
+        $address1 = sanitize($_POST['address1'], null);
+        $city = sanitize($_POST['city'], null);
+        $state = sanitize($_POST['state'], null);
+        $zip = sanitize($_POST['zip'], null);
+        /* unsanitized */
+        $password = $_POST['password'];
+        $password_confirm = $_POST['password2'];
 
+        if ($password != $password_confirm) {
+            $alert = 1;
+            $type = "alert-danger text-danger alert-dismissible";
+            $text = "Your passwords must match!";
+        }
+        else {
+            /* Salt and encrypt password into database */
+            $salt = salt();
+            $password = $password .$salt;
+            $encryptedPassword = md5($password);
+            $textPassword = "$encryptedPassword";
+            $sqlFail = 0;
+            if ($sqlFail == 0) {
+                $sql = "INSERT INTO users (FirstName,LastName,username,Password,salt, email,address1,City,state, zipcode, joined)
+VALUES ('$fname', '$lname','$username','$textPassword','$salt','$email','$address1','$city','$state','$state', NOW())";
                 try {
                     $handler->query($sql);
                     $alert = 1;
@@ -46,19 +50,31 @@
                     $text = $e->getMessage();
                 }
             }
-        } elseif (isset($_POST['logon'])) {
-            $alert = 1;
-            $type = "alert-success text-success alert-dismissible";
-            $text = "Logon submitted!";
-        } else {
-            $alert = 1;
-            $type = "alert-danger text-danger";
-            $text = "an unknown result was triggered in" . $_SERVER['PHP_SELF'];
+            elseif ($sqlFail = 1) {
+                $alert = 1;
+                $type = "alert-danger text-danger alert-dismissible";
+                $text = "Error: " . $errorType;
+                unset($sqlFail);
+            }
+            else {
+                $alert = 1;
+                $type = "alert-danger text-danger alert-dismissible";
+                $text = "Error: an unknown error occurred";
+            }
+
+
         }
+    } elseif (isset($_POST['logon'])) {
+        $alert = 1;
+        $type = "alert-success text-success alert-dismissible";
+        $text = "Logon submitted!";
     } else {
+        $alert = 1;
+        $type = "alert-danger text-danger";
+        $text = "an unknown result was triggered in" . $_SERVER['PHP_SELF'];
     }
-    echo '<pre>';
-var_dump($_POST);
-echo '</pre>';
-header("location:../index.php");
+} else {
+}
+
+//header("location:../index.php");
 ?>
--- bin/functions.php
+++ bin/functions.php
@@ -19,12 +19,40 @@ return $page;
 }
 
 /**
+ * Added security no params this function creates an alphanumeric(4) to add to existing password to increase security.
 */
-
+function salt() {
+    $char = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    $newsalt = substr(str_shuffle($char),0,4);
+    return $newsalt;
+}
 
 /**
+ * Function to verify that user doesn't exist otherwise determining unique users would fail
+ * $user = username to check
+ * $handler = sql connect string from formHandler
 */
-function getTitle($page){
-    $pretext = "Welcome to ACME: ";
-
+function chkuser($user, $handler) {
+    include INC_ROOT . 'bin/sqlConnector.php';
+    $query = $handler->query("SELECT * FROM users");
+    while($r = $query->fetch()) {
+        if ($r['username'] == $user) {
+            return true;
+        }
+        else {
+           return false;
+        }
+    }
+}
+function chkemail($email) {
+    include INC_ROOT . 'bin/sqlConnector.php';
+    $query = $handler->query("SELECT * FROM users");
+    while($r = $query->fetch()) {
+        if ($r['email'] == $email) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
 }
