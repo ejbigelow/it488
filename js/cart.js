2b6d799353167bb25f02dad3309cdd2be7741d8b
Eric Bigelow <eric.bigelow@me.com>
2017-05-18 14:44:24 -0400

    working checkout with shipping 1 item

--- js/cart.js
+++ js/cart.js
@@ -5,21 +5,20 @@
 
         var qty1 = Number(document.getElementById('qty').value);
         var price1 = Number(document.getElementById('price').value);
+        var shipping = Number(document.getElementById('cshipping').value);
 
-        var qty2 = Number(document.getElementById('qty1').value);
-        var price2 = Number(document.getElementById('price1').value);
+
 
 
         var sampletax = 0.06;
         var ptotal=qty1*price1;
-        var ptotal1=qty2*price2;
         var ptotalFix = ptotal.toFixed(2);
         var totalTax = ptotal*0.06.toFixed(2);
 
         document.getElementById("itemTotal").innerHTML = '$' + ptotalFix;
         document.getElementById("displaySubtotal").innerHTML = '$' + ptotalFix;
-        document.getElementById("displaySalesTax").innerHTML = totalTax.toFixed(2);
-        document.getElementById("displayTotal").innerHTML = ptotal+totalTax;
+        document.getElementById("displaySalesTax").innerHTML = '$' + totalTax.toFixed(2);
+        document.getElementById("displayTotal").innerHTML = shipping + ptotal + totalTax;
 
     }
 <!--End java script sum-->
---
+++ lib/checkout-.php
@@ -0,0 +1,122 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <title>ACME llc. Order form:
+        Checkout        </title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css">
+    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="//code.jquery.com/ui/1.11.4/themes/smoothness/jquery-ui.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+    <script src="//code.jquery.com/jquery-1.10.2.js"></script>
+    <script src="//code.jquery.com/ui/1.11.4/jquery-ui.js"></script>
+    <link href="css/custom.css" rel="stylesheet">
+
+
+</head>
+<body>
+
+
+<div class="container ">
+    <div class="row cart-head">
+        <div class="container">
+            <div class="row">
+                <p></p>
+            </div>
+            <div class="row">
+                <div style="display: table; margin: auto;">
+                    <span class="step step_complete"> <a href="index.html" class="check-bc">Home page</a></span>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="row cart-body">
+        <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12 col-md-push-6 col-sm-push-6">
+            <div class="panel panel-info">
+                <div class="panel-heading">
+                    Review Order <div class="pull-right"><small><a class="afix-1" href="index.html">cancel order</a></small></div>
+                </div>
+                <div class="panel-body">
+                    <!-- Loop here -->
+                    <div class="row">
+                        <div id="itemPictureHeader" class="col-lg-3">
+                        </div>
+                        <div id="itemNameHeader" class="col-lg-2">
+                            Name
+                        </div>
+                        <div id="itemPriceHeader" class="col-lg-2">
+                            Unit Price
+                        </div>
+                        <div id="itemQtyHeader" class="col-lg-2">
+                            Quantity
+                        </div>
+                        <div id="itemTotalHeader" class="col-lg-2">
+                            total
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div id="itemPicture" class="col-lg-3">
+                            <img class="img-responsive" src="../img/242x200.png" alt="sample img"/>
+                        </div>
+                        <div id="itemName" class="col-lg-2">
+                            whirlygig
+                        </div>
+                        <div id="itemPrice" class="col-lg-2">
+                            <!--Need unchangeable price implemented spanning price to signify to JS code this value we multiply across -->
+                            <input type="text" class="form-control" name="price" id="price" value="1.00" readonly/><span></span>
+                        </div>
+                        <div id="itemQty" class="col-lg-2">
+                            <input  onchange="findTotal()"  type="text" class="form-control" id="qty" placeholder="Qty"/>
+                        </div>
+                        <div id="itemTotal" class="col-lg-2">
+                        </div>
+                    </div>
+                    <!--End any loops here-->
+                    <hr />
+                    <!-- Totals -->
+                    <div class="row" id="Totals">
+                        <!--leave left blank-->
+                        <div class="col-lg-6"></div>
+                        <div class="col-lg-3 text-right">
+                            <p>Sub Total:</p>
+                            <p>Shipping:</p>
+                            <p>*Sales Tax(6%):</p>
+                            <p>Total: $</p>
+                        </div>
+                        <div class="col-lg-3 text-right">
+                            <p id="displaySubtotal">&nbsp;</p>
+                            <form>
+                                <select class="form-control" onchange="findTotal()" id="cshipping">
+                                    <option value="0.00" selected>Free (+&dollar;0.00)</option>
+                                    <option value="10.00">First Class (+&dollar;10.00)</option>
+                                    <option value="20.00">Next Day Air (+&dollar;20.00)</option>
+                                </select>
+                            </form>
+                            <p id="displaySalesTax">&nbsp;</p>
+                            <p id="displayTotal">&nbsp;</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <p>*Based on your State's tax regulations we will include this amount into your fee on your State's behalf.</p>
+        </div>
+    </div>
+</div>
+</div>
+</div>
+<div class="col-lg-6 col-md-6 col-sm-6 col-xs-12 col-md-pull-6 col-sm-pull-6">
+
+</div>
+<div class="row cart-footer">
+
+</div>
+
+
+
+</body>
+<script src="../js/cart.js"></script>
